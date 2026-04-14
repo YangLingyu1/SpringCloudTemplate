f06f907177c03aad5d1975d6dd8a589f56a6b71b
tjxt <tjxt@itcast.cn>
2026-04-14 17:07:08 +0800

    实现了Sentinel熔断限流降级

--- ai-service/src/main/java/com/lingyu/AIServiceApplication.java
+++ ai-service/src/main/java/com/lingyu/AIServiceApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class OrderServiceApplication {
+public class AIServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderServiceApplication.class, args);
+        SpringApplication.run(AIServiceApplication.class, args);
     }
 
 }
